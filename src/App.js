c8e5a77aad69945cded4ff54c54b5adc090e39dc
Ravishek Singh <ravishekstar@gmail.com>
2025-06-14 20:17:16 +0530

    route-protect

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ function App() {
                                     <Route path="/bdgwin" element={<BDGWin />} />
 
                                     {/* Protected Routes */}
-                                    <Route>
+                                    <ProtectedRoute>
                                         <Route path="/" element={<Home />} />
                                         <Route path="/profilepage" element={<ProfilePage />} />
                                         <Route path="/wallet" element={<Wallet />} />
@@ -207,7 +207,7 @@ function App() {
                                         <Route path="/SportsGame" element={<SportsGame />} />
                                         <Route path="/casino-games" element={<CasinoGames />} />
                                         <Route path="/collect-reward" element={<CollectReward />} />
-                                    </Route>
+                                    </ProtectedRoute>
 
                                     {/* Default Redirect */}
                                     <Route path="*" element={<Navigate to="/login" replace />} />
